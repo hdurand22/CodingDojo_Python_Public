20c7d8a3d2c2f590dc9c39eae1448a84d05b8b5c
hdurand22 <hdurand22@gmail.com>
2021-06-08 12:54:25 -0700

    adding method chaining

--- Python/fundamentals/OOP/user.py
+++ Python/fundamentals/OOP/user.py
@@ -6,39 +6,43 @@ class User:
     
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
     
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
         print(f"{self.name}, Balance: {self.account_balance}")
+        return self
     
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.make_deposit(amount)
+        return self
 
 user1 = User("Hayden", "hayden@hayden.com")
 user2 = User("Mom", "mom@mom.com")
 user3 = User("Dad", "dad@dad.com")
 
-user1.make_deposit(100)
-user1.make_deposit(100)
-user1.make_deposit(100)
-user1.make_withdrawal(200)
-user1.display_user_balance()
+user1.make_deposit(100).make_deposit(100).make_deposit(100).make_withdrawal(200).transfer_money(user3, 50).display_user_balance()
+# user1.make_deposit(100)
+# user1.make_deposit(100)
+# user1.make_withdrawal(200)
+# user1.display_user_balance()
 
-user2.make_deposit(100)
-user2.make_deposit(50)
-user2.make_withdrawal(50)
-user2.make_withdrawal(10)
-user2.display_user_balance()
+user2.make_deposit(100).make_deposit(50).make_withdrawal(50).make_withdrawal(10).display_user_balance()
+# user2.make_deposit(50)
+# user2.make_withdrawal(50)
+# user2.make_withdrawal(10)
+# user2.display_user_balance()
 
-user3.make_deposit(500)
-user3.make_withdrawal(25)
-user3.make_withdrawal(25)
-user3.make_withdrawal(25)
-user3.display_user_balance()
+user3.make_deposit(500).make_withdrawal(25).make_withdrawal(25).make_withdrawal(25).display_user_balance()
+# user3.make_withdrawal(25)
+# user3.make_withdrawal(25)
+# user3.make_withdrawal(25)
+# user3.display_user_balance()
 
-user1.transfer_money(user3, 50)
-user1.display_user_balance()
-user3.display_user_balance()
+# user1.transfer_money(user3, 50)
+# user1.display_user_balance()
+# user3.display_user_balance()
