2bcf2a92dc2d9e6fd23cc76346d53771f0f83fbb
hdurand22 <hdurand22@gmail.com>
2021-06-08 18:59:05 -0700

    modularized bank account, allow multiple accounts per user

--- Python/fundamentals/OOP/bank_account.py
+++ Python/fundamentals/OOP/bank_account.py
@@ -1,11 +1,12 @@
 class BankAccount:
     
-    bank_name = "First National Dojo"
-    all_accounts = []
-    def __init__(self, int_rate, balance=0): 
+    # bank_name = "First National Dojo"
+    # all_accounts = []
+    def __init__(self, int_rate, balance, account_name): 
         self.int_rate = int_rate
         self.balance = balance
-        BankAccount.all_accounts.append(self)
+        self.account_name = account_name
+        # BankAccount.all_accounts.append(self)
     
     def deposit(self, amount):
         self.balance += amount
@@ -18,17 +19,17 @@ class BankAccount:
             print("Insufficient Funds")
         return self
 
-    def display_account_info(self):
-        print(f"Balance: {self.balance}")
+    def display_account_info(self, name):
+        print(f"{name}'s account balance: {self.balance}")
         return self
 
     def yield_interest(self):
         self.balance += (self.balance*self.int_rate)
         return self
     
-    @classmethod
-    def all_balances(cls):
-        print(cls.all_accounts)
+    # @classmethod
+    # def all_balances(cls):
+    #     print(cls.all_accounts)
 
     @staticmethod
     def can_withdraw(balance, amount):
@@ -37,10 +38,10 @@ class BankAccount:
         else:
             return True
 
-account1 = BankAccount(0.02)
-account2 = BankAccount(0.03, 500)
+# account1 = BankAccount(0.02)
+# account2 = BankAccount(0.03, 500)
 
-account1.deposit(200).deposit(200).deposit(50).withdraw(200).yield_interest().display_account_info()
-account2.deposit(200).deposit(200).withdraw(50).withdraw(200).withdraw(50).withdraw(10).yield_interest().display_account_info()
+# account1.deposit(200).deposit(200).deposit(50).withdraw(200).yield_interest().display_account_info()
+# account2.deposit(200).deposit(200).withdraw(50).withdraw(200).withdraw(50).withdraw(10).yield_interest().display_account_info()
 
-BankAccount.all_balances()
+# BankAccount.all_balances()
--- Python/fundamentals/OOP/user.py
+++ Python/fundamentals/OOP/user.py
@@ -1,43 +1,118 @@
+import bank_account
+
 class User:
-    def __init__(self, name, email_address):
-        self.name = name
+    def __init__(self, username, email_address):
+        self.username = username
         self.email_address = email_address
-        self.account_balance = 0
+        self.accounts = [] # Create empty list of accounts
+        # self.account_balance = 0
     
-    def make_deposit(self, amount):
-        self.account_balance += amount
+    def make_deposit(self, name, amount):
+        # Look through list of user's accounts
+        # Find account with same name as account_name
+        # Deposit amount to that account
+        # If account isn't found, print an error
+
+        has_account = False # Check for nonexistent account
+        for account in self.accounts:
+            if account.account_name == name:
+                has_account = True
+                account.deposit(amount)
+                print(f"Deposit of ${amount} to {name} complete")
+
+        if has_account == False:
+                print(f"Account name '{name}' not found")
+    
         return self
     
-    def make_withdrawal(self, amount):
-        self.account_balance -= amount
+    def make_withdrawal(self, name, amount):
+        # Look through list of user's accounts
+        # Find account with same name as account_name
+        # Withdraw amount from that account
+        # If account isn't found, print an error
+
+        has_account = False # Check for nonexistent account
+        for account in self.accounts:
+            if account.account_name == name:
+                has_account = True
+                account.withdraw(amount)
+                print(f"Withdrawal of ${amount} from {name} complete")
+
+        if has_account == False:
+                print(f"Account name '{name}' not found")
+    
         return self
 
-    def display_user_balance(self):
-        print(f"{self.name}, Balance: {self.account_balance}")
+    def display_user_balance(self, name):
+        # Look through list of user's accounts
+        # Find account with same name as account_name
+        # Get account balance
+        # If account isn't found, print an error
+
+        has_account = False # Check for nonexistent account
+        for account in self.accounts:
+            if account.account_name == name:
+                account.display_account_info(name)
+                has_account = True
+
+        if has_account == False:
+                print(f"Account name '{name}' not found")
+    
         return self
     
-    def transfer_money(self, other_user, amount):
-        self.account_balance -= amount
-        other_user.make_deposit(amount)
+    def transfer_money(self, transfer_from, other_user, transfer_to, amount):
+        self.make_withdrawal(transfer_from, amount)
+        other_user.make_deposit(transfer_to, amount)
         return self
+    
+    def make_account(self, int_rate=0.02, init_deposit=0, account_name="New Account"):
+        # Make a new bank account with a name, interest rate, and initial deposit
+        # Add account to user's accounts dictionary
 
+        # Check to see if user has multiple accounts and create a default name if 
+        if len(self.accounts) != 0:
+            account_name = "New Account " + str(len(self.accounts)+1)
+        self.accounts.append(bank_account.BankAccount(int_rate, init_deposit, account_name))
+        return self
+    
+
+# ========== TESTS =======================================================
+
+# Create multiple users
 user1 = User("Hayden", "hayden@hayden.com")
 user2 = User("Mom", "mom@mom.com")
 user3 = User("Dad", "dad@dad.com")
 
-user1.make_deposit(100).make_deposit(100).make_deposit(100).make_withdrawal(200).transfer_money(user3, 50).display_user_balance()
-# user1.make_deposit(100)
-# user1.make_deposit(100)
-# user1.make_withdrawal(200)
-# user1.display_user_balance()
+# Create multiple accounts for a single user
+user1.make_account(0.01, 1000, "Checking 2")
+user1.make_account(0.01, 1000)
+user1.make_account(0.01, 1000)
+user2.make_account(0.05, 600, "Fun Munny")
 
-user2.make_deposit(100).make_deposit(50).make_withdrawal(50).make_withdrawal(10).display_user_balance()
-# user2.make_deposit(50)
-# user2.make_withdrawal(50)
-# user2.make_withdrawal(10)
-# user2.display_user_balance()
+# Deposit to specific account
+user1.make_withdrawal("New Account 2", 100)
+user1.display_user_balance("New Account 2")
+user2.make_deposit("Fun Munny", 2000)
+user2.display_user_balance("Fun Munny")
 
-user3.make_deposit(500).make_withdrawal(25).make_withdrawal(25).make_withdrawal(25).display_user_balance()
+# Transfer
+user1.display_user_balance("Checking 2")
+user2.transfer_money("Fun Munny", user1, "Checking 2", 500)
+user1.display_user_balance("Checking 2")
+
+# user1.make_deposit(100).make_deposit(100).make_deposit(100).make_withdrawal(200).transfer_money(user3, 50).display_user_balance()
+# # user1.make_deposit(100)
+# # user1.make_deposit(100)
+# # user1.make_withdrawal(200)
+# # user1.display_user_balance()
+
+# user2.make_deposit(100).make_deposit(50).make_withdrawal(50).make_withdrawal(10).display_user_balance()
+# # user2.make_deposit(50)
+# # user2.make_withdrawal(50)
+# # user2.make_withdrawal(10)
+# # user2.display_user_balance()
+
+# user3.make_deposit(500).make_withdrawal(25).make_withdrawal(25).make_withdrawal(25).display_user_balance()
 # user3.make_withdrawal(25)
 # user3.make_withdrawal(25)
 # user3.make_withdrawal(25)
